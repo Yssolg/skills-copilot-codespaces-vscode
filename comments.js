ce4b6bf8a081c1233af736f6dd19999ace5d31a0
Yssolg <yssolg@gmail.com>
2025-01-27 23:11:30 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,9 @@
+// Create web server
+// 1. Parse URL
+// 2. Parse cookies
+// 3. Parse body
+// 4. Check if user is logged in
+// 5. Check if user has permission to view comments
+// 6. If user has permission, show comments
+// 7. If user doesn't have permission, show error message
+// 8. If user is not logged in, redirect to login page
